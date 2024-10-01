080a4f6d86e3389d0f0dfd09c4b377f12362e168
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2024-10-02 01:41:20 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import Dishes from "./components/Dishes";
+import About from "./components/About";
+import Mission from "./components/Mission";
+import Expertise from "./components/Expertise";
+import Review from "./components/Review";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline bg-white">
-      Hello world!
-    </h1>
-    </div>
-  );
+   <main className="overflow-y-hidden  text-neutral-200 ">
+    
+    <HeroSection />
+    <Navbar />
+    <Dishes />
+    <About />
+    <Mission />
+    <Expertise />
+    <Review />
+    <ContactSection />
+    <Footer />
+    
+   </main>
+  )
 }
 
 export default App;
